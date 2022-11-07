ceea429531b8bb0ef9b89e075a7d0a65130d2780
austinzaw <zawmyohtwe1080@gmail.com>
2022-11-07 15:08:50 +0630

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+    menu.classList.toggle('move')
+}
+var swiper = new Swiper(".review-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
